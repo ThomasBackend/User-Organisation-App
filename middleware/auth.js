506da86542c7db9df731cc4463741d2f5891e5d9
ThomasBackend <137883152+ThomasBackend@users.noreply.github.com>
2024-07-08 11:54:54 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,14 +7,19 @@ export default function authenticateToken(req, res, next) {
     try{
     const authHeader = req.headers['authorization'];
 
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Authorization header is missing' });
+  }
 
-    if(!authHeader){
-      return res.status(401).json({error : 'authorization is missing from headers'})
-    }
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token is missing' });
+  }
   
   
     // Verify the token
-    jwt.verify(authHeader, process.env.SECRET_KEY, (err, user) => {
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
       if (err) {
         return res.sendStatus(403).json({error :"Forbidden. Token is not valid"}); 
       }
