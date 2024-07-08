403658e40e6e4491a34401ed7867c25ff5b8dcc7
ThomasBackend <137883152+ThomasBackend@users.noreply.github.com>
2024-07-08 12:33:57 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ router.post('/register', authController.register);
 router.post('/login',authController.login)
 
 router.get('*', (req,res) => {
-    return res.status(200).send('This route is not in use.')
+    return res.status(404).send('This route is not in use.')
   })
 
 export default router;
